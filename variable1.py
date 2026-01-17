e895861ce0e4ede218d2192f4328a68d28a72b20
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-01-17 17:00:27 +0300

    overwrote python print end parameter

--- variable1.py
+++ variable1.py
@@ -1,3 +1,4 @@
-# Ask user for their name and greet them
-name = input("What is your name?")
-print("Hello " + name + "!")
+# Ask user for their name and greet themj
+name = input("What is your name?  ")
+print("Hello, ", end = "" )
+print(name)
