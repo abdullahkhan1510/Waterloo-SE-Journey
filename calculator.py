9448768d74bf3396e771c21dce660951b086fd11
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-01-20 18:12:07 +0300

    integer first use

---
+++ calculator.py
@@ -0,0 +1,4 @@
+x =input("Enter your x value: ")
+y = input("Enter your y value: ")
+z = int(x) + int(y)
+print(z)
--- variable1.py
+++ variable1.py
@@ -2,5 +2,4 @@
 name = input("What is your name ? ").strip().title()
 # split input into two parts and take first part
 first, last = name.split(" ")
-
 print(f"Hello, {first}")
