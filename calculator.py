81563b553c1f11e7600de2c33ff9e2aaec41c83b
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-01-20 19:13:24 +0300

    use round function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
-x = int(input("Enter your x value: "))
-y = int(input("Enter your y value: "))
-print(x + y)
+x = float(input("Enter your x value: "))
+y = float(input("Enter your y value: "))
+z = round(x+y)
+print(z)
