90bd262fcf6209b455d9e1a114363bd0a5cd4525
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-01-24 16:36:34 +0300

    e = mc^2

--- week0.py
+++ week0.py
@@ -1,3 +1,3 @@
-msg = input("What is your input?")
-msg = msg.replace(" ", "...")
-print(msg)
+mass = int(input("What is your mass?"))
+energy = mass * 300000000**2
+print("Your energy is", energy ,"Joules")
