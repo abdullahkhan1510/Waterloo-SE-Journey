b8f0d7d78ff59c15b6a0ccbff82711bd95a40ebb
abdullahkhan1510 <abdullahimadkhan15@gmail.com>
2026-01-27 20:17:49 +0300

    take a number with a $ sign as input, and convert that number into a decimal

--- week0.py
+++ week0.py
@@ -1,5 +1,11 @@
-def addten():
-    number = int(input("What is your number?"))
-    result = number + 10
-    return result
-print(addten())
+def make_number():
+    num = input("What is your number, with a dollar sign?")
+    clean_num = num.replace("$","")
+    return clean_num
+def final_num(n):
+    num = float(n) / 100
+    return num
+
+noperc = make_number()
+dec = final_num(noperc)
+print(dec)
